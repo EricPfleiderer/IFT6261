9ca852f1b2128f0632857241d84b9bda01a563b5
anon <e.pfleiderer@hotmail.com>
2022-04-07 21:36:24 -0400

    Starting experiments

--- driver.py
+++ driver.py
@@ -1,7 +1,7 @@
 import os
 import pickle
 import logging
-import json
+import csv
 from datetime import datetime
 
 import torch
@@ -19,32 +19,40 @@ utils.set_logging(logging_level)
 
 def run_experiment(x_index=788, trained_classifier_path=None, root='outputs/experiments/'):
 
+    now = datetime.now()
+    full_path = root + 'Experiment_' + str(now) + '/' # Full path to current experiment
+    os.makedirs(full_path)
+
     # Train from scratch
     if trained_classifier_path is None:
         params = dict(**CNN_space, **configs)
         trainable = TorchTrainable(params)
         trainable.train()
-        os.makedirs(root+'models/')
-        pickle.dump(trainable, open(root+'models/' + 'classifier.json', 'wb'))
-        pickle.dump(params, open(root+'models/' + 'classifier_params.json', 'wb'))
+        os.makedirs(full_path+'models/')
+        pickle.dump(trainable, open(full_path+'models/' + 'classifier.json', 'wb'))
+
+        # open file for writing, "w" is writing
+        w = csv.writer(open(full_path + 'classifier_params.csv', "w"))
+        for key, val in params.items():
+            w.writerow([key, val])
 
     # Load from save
     else:
         trainable = pickle.load(open(trained_classifier_path, 'rb'))
-
-    now = datetime.now()
-    full_path = root + 'Experiment_' + str(now) + '/'
-    os.makedirs(full_path)
 
     trainable.plot_history(path=full_path)
 
     # Attack sample (single out of sample image and target)
     x, y = trainable.test_loader.dataset[x_index][0][0], trainable.test_loader.dataset[x_index][1]
 
-    ga = GeneticAttack(x, y, trainable, N=500, epochs=100, selective_pressure=0.4, asexual_repro=1, epsilon=0.05,
-                       uncertainty_power=2, sameness_power=2)
+    params = GA_space
+    ga = GeneticAttack(x, y, trainable, **params)
     ga.plot_history(path=full_path)
-    pickle.dump(ga, open(root + 'models/' + 'ga.json', 'wb'))
+    pickle.dump(ga, open(full_path + 'models/' + 'ga.json', 'wb'))
+    # open file for writing, "w" is writing
+    w = csv.writer(open(full_path + 'ga_params.csv', "w"))
+    for key, val in params.items():
+        w.writerow([key, val])
 
     original_prediction_dist = trainable(x)
     print(f'The model thinks this is an image of a {torch.argmax(original_prediction_dist)} with a confidence of {torch.max(original_prediction_dist)}')
--- src/hyperparams.py
+++ src/hyperparams.py
@@ -13,3 +13,12 @@ CNN_space = {
     }
 }
 
+GA_space = {
+    'epochs': 200,
+    'N': 50,
+    'selective_pressure': 0.4,
+    'asexual_repro': 1.0,
+    'epsilon': 0.025,
+    'uncertainty_power': 2,
+    'sameness_power': 2,
+}
